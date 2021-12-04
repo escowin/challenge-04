faa44c77c72cbff08025399a8e82677edb4bb874
escowin <edwin@escowinart.com>
2021-12-04 12:19:31 -0600

    html & js pseudocode commentary

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//clicking start button starts timer
+
+//answering question presents another question
+    //but if answering incorrectly, time is deducted from timer
+
+//quiz ends after answering all questions
+    //or if timer reaches 0
+
+//game over coda, save initials & score
