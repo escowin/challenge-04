07c785652789f5804127daded03f9ac91b969c23
escowin <edwin@escowinart.com>
2021-12-05 16:58:23 -0600

    syncing up classes & ids across index, css, and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,87 @@
-//clicking start button starts timer
+//relevant IDs & elements from html
+var highScoresEl = document.querySelector(".highScores");
+var timerEl = document.querySelector(".timer");
+var heroEl = document.querySelector(".hero");
+var btnStartEl = document.querySelector(".btn-start");
+var answerEl = document.querySelector(".answer");
 
-//answering question presents another question
-    //but if answering incorrectly, time is deducted from timer
+// timer
+var timeLeft = 60;
 
-//quiz ends after answering all questions
-    //or if timer reaches 0
+    // countdown function that includes a time deduction penalty
+
+    //quiz ends after answering all questions
+    //or if timer reaches 0 
+
+//Array of Questions and Answers from which the runQuiz() will pull from.
+var questions = [
+    {
+        question: "Question One?",
+        answers: [
+            "Answer One",
+            "Answer Two",
+            "Answer Three",
+            "Answer Four"
+        ],
+        correctAnswer: "Answer Two"
+    },
+    {
+        question: "Question Two?",
+        answers: [
+            "Answer One",
+            "Answer Two",
+            "Answer Three",
+            "Answer Four"
+        ],
+        correctAnswer: "Answer Four"
+    },
+    {
+        question: "Question Three?",
+        answers: [
+            "Answer One",
+            "Answer Two",
+            "Answer Three",
+            "Answer Four"
+        ],
+        correctAnswer: "Answer Four"
+    },
+    {
+        question: "Question Four?",
+        answers: [
+            "Answer One",
+            "Answer Two",
+            "Answer Three",
+            "Answer Four"
+        ],
+        correctAnswer: "Answer One"
+    }
+]
+
+var questionCounter = 0
+var score = 0
+
+//functions to run the quiz. show questions on index via dynamically generated html here
+
+var runQuiz = function() {
+
+};
+// function to check answer. answer correctly, move on to next.
+    // answer incorrectly, time deduction.
+
+
+// function to show correct answer
 
 //game over coda, save initials & score
+var viewHighScores = function() {
+    welcomeSection.remove();
+    scpreTitle.textContent = "h i g h  s c o r e:";
+};
+
+
+//Event Listeners to run above functions
+btnStartEl.addEventListener('click', function() {
+    runQuiz();
+});
+highScoresEl.addEventListener('click', function() {
+    viewHighScores();
+});
