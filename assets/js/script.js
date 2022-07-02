1045cff5e25b6e9f17449e555d3223829a479c51
escowin <edwin@escowinart.com>
2022-07-02 12:57:25 -0500

    js | //pseudocode quiz logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-// redux | js logic
 // dom elements
 var startButtonEl = document.querySelector('#start-button');
+var saveButtonEl = document.querySelector('#save-button');
+var timeButtonEl = document.querySelector('#time-button');
+var playAgainButtonEl = document.querySelector('#play-again-button');
+
+// logic.timer
+var countdownTimer = function() {
+    // logic.countdown from 10s
+    console.log("timer countdown from 10s")
+    // logic.show the countdown to user
+    // logic.time reaches 0 triggers endQuiz()
+};
 
 // logic.quiz
 var startQuiz = function() {
-    // questions array
+    // array.questions
     var questionsArray = [
         {
             question: "HTML?",
@@ -25,48 +35,38 @@ var startQuiz = function() {
             answer: "b"
         },
        ];
-
     for(var i = 0; i < questionsArray.length; i++) {
         console.log(questionsArray[i]);
-        console.log(i);
-        console.log(questionsArray[i] + " is at " + i + " index");
     }
-    // current question
+    // logic.questions.current-question
     var currentQuestion = questionsArray;
     // console.log(currentQuestion.length);
+    // logic.answer question
+        // logic.correctly answer, go to next question
+        // logic.incorrectly answer, deduct time && go to next question
+        // logic.present next question
+            // logic.if all questions answered || time reaches 0, triggers endQuiz()
 };
 
-
-// for(initial expression; condition; increment expression) {
-//     statement
-//  }
-
-
-// logic.timer
-var countdownTimer = function() {
-    console.log("timer countdown from 10s")
+var endQuiz = function() {
+    console.log('quiz is over')
 };
 
-// event listener
+// logic.saving
+var saveHighScore = function() {
+    // logic.save score
+    console.log('save high score')
+    // logic.show saved score in an <ol>
+    // logic.play again option
+};
+
+// event listeners
 startButtonEl.addEventListener('click', () => {
     startQuiz();
     countdownTimer();
 });
 
-
-//  TIMER
-// click event | timer starts
-// function | timer
-
-// click event | answer question
-
-
-// QUESTIONS
-// function | questions array
-// function | correct v incorrect
-// function | incorrect penalty (time deduction)
-// function | game over mechanics (answered everything || time is 0)
+saveButtonEl.addEventListener('click', saveHighScore);
 
 // SCORE
 // function | saving (inputting initials & score. score = time remaining. possibly incorporate a point mechanism in correct penalty: total no. of questions as total point base. each incorrect answer deducts a point. remaining points is saved into score.)
-// 
