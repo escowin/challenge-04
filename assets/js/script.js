a93e8ffede498cd281353feb80c40924b567fbc5
escowin <edwin@escowinart.com>
2022-07-02 15:13:39 -0500

    pause, resume js:62

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,55 +10,92 @@ var countdownTimer = function() {
     console.log("timer countdown from 10s")
     // logic.show the countdown to user
     // logic.time reaches 0 triggers endQuiz()
+    // logic.if all questions are answered before countdown reaches 0, remaining time goes to saveHighScore()
 };
+
+var questions = [
+    {
+        question: "HTML?",
+        choices: {
+            a: "yay", 
+            b: "nay", 
+            c: "abstain"
+        },
+        answer: "a"
+    },
+    {
+        question: "CSS?",
+        choices: {
+            a: "what", 
+            b: "who", 
+            c: "why"
+        },
+        answer: "b"
+    },
+    ];
 
 // logic.quiz
 var startQuiz = function() {
-    // array.questions
-    var questionsArray = [
-        {
-            question: "HTML?",
-            choices: {
-                a: "yay", 
-                b: "nay", 
-                c: "abstain"
-            },
-            answer: "a"
-        },
-        {
-            question: "CSS?",
-            choices: {
-                a: "what", 
-                b: "who", 
-                c: "why"
-            },
-            answer: "b"
-        },
-       ];
-    for(var i = 0; i < questionsArray.length; i++) {
-        console.log(questionsArray[i]);
-    }
-    // logic.questions.current-question
-    var currentQuestion = questionsArray;
+    // logic.change <main><start-quiz/></main> to <main><quiz-inprogress/></main>
+
+    var currentQuestion = questions;
+    // logic.loop through questions[] for an unaswered question:
+        for(var i = 0; i < questions.length; i++) {
+        }
+        // logic.select one {question} from [questions] 
+        var getQuestion = 'get question';
+        console.log(getQuestion + ' is working');
+        var displayQuestion = function (){
+            // logic.display selected {question} in DOM <divs>
+                // <main>
+                    // <#quiz>
+                        // <#question>
+                        // <#choices>
+                            // <.choice/>            
+        };
+        var answerQuestion = function(){
+        // logic.user selects <.choice> from <#choices>
+        // triggers nextQuesition()
+                nextQuestion();
+
+        };
+    // >save point<
+    var nextQuestion = "next question is here"
+
+    
     // console.log(currentQuestion.length);
     // logic.answer question
         // logic.correctly answer, go to next question
         // logic.incorrectly answer, deduct time && go to next question
         // logic.present next question
             // logic.if all questions answered || time reaches 0, triggers endQuiz()
+    endQuiz();
 };
 
 var endQuiz = function() {
     console.log('quiz is over')
+    // score calculations
+    var score = function() {
+
+        // logic.remaining countdownTimer()
+        // logic.user-input initials()
+    };
+    console.log('endQuiz() score is "initials + countdownTimer"');
+
+    // logic.saving
+    var saveHighScore = function() {
+        // logic.save score
+        console.log('save high score')
+        // logic.saving
+        // logic.saving high score is displayed by user-input.
+            // regex. /2 Initials, A-Z/
+        // logic.score is (initials + " | " + coundtownTimer)
+        // logic.show saved score in an <ol>
+        // logic.play again option
+    };
 };
 
-// logic.saving
-var saveHighScore = function() {
-    // logic.save score
-    console.log('save high score')
-    // logic.show saved score in an <ol>
-    // logic.play again option
-};
+
 
 // event listeners
 startButtonEl.addEventListener('click', () => {
@@ -66,7 +103,7 @@ startButtonEl.addEventListener('click', () => {
     countdownTimer();
 });
 
-saveButtonEl.addEventListener('click', saveHighScore);
+// saveButtonEl.addEventListener('click', saveHighScore);
 
 // SCORE
 // function | saving (inputting initials & score. score = time remaining. possibly incorporate a point mechanism in correct penalty: total no. of questions as total point base. each incorrect answer deducts a point. remaining points is saved into score.)
