98e9100f7104a68e75fcd13f5c2dc7fb01eb1cfb
escowin <edwin@escowinart.com>
2022-07-29 18:20:10 -0500

    js:84 | + question array is called

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,41 @@ var timerEl = document.querySelector('#timer');
 
 // data.user-input
 var inititals = "XX";
+var timer = 10;
 
 // data.questions-array
-var questions = {
-    question: "html question",
-    answer: "this is the answer"
-};
+var questions = [
+    {
+        question: "this is question 1",
+        answers: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correct: "b"
+    },
+    {
+        question: "this is question 2",
+        answers: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correct: "a"
+    },
+];
 
-// logic.countdown
+// logic.countdown-timer
 var countdownTimer = function() {
-    var timeLeft = 10;
-
     var timeInterval = setInterval(function() {
-        if (timeLeft > 0) {
-            timerEl.textContent = timeLeft + 's';
-            timeLeft--;
-        } else if (timeLeft === 0) {
-            timerEl.textContent = timeLeft;
-            timeLeft--;
+        if (timer > 0) {
+            timerEl.textContent = timer + 's';
+            timer--;
+        } else if (timer === 0) {
+            timerEl.textContent = timer;
+            timer--;
         } else {
             clearInterval(timeInterval);
             endQuiz();
@@ -31,9 +48,8 @@ var countdownTimer = function() {
     }, 1000)
 };
 
-// logic.display
+// logic.display-questions
 var displayQuestions = function() {
-    // to-do: replace innerText placeholder string w/ object key-values
     var questionsContainerEl = document.createElement("section");
     questionsContainerEl.className = "container";
     questionsContainerEl.setAttribute("id", "questions-container");
@@ -53,18 +69,43 @@ var displayQuestions = function() {
     quizContainerEl.appendChild(questionsContainerEl);
     questionsContainerEl.append(questionEl, answersEl);
     answersEl.append(answerEl);
-}
+
+    answerEl.addEventListener("click", nextQuestion);
+};
 
 // logic.start-quiz
 var startQuiz = function() {
     rulesContainerEl.remove();
+    getQuestion();
     displayQuestions();
-}
+    countdownTimer();
+};
+
+var getQuestion = function() {
+    for (var i = 0; i < questions.length; i++) {
+
+        if (questions.length > 0) {
+            console.log("works")
+        } else {
+            console.log("no questions");
+            break;
+        }
+    }
+
+    // bug
+    var currentQuestion = questions[i];
+        console.log(currentQuestion);
+
+};
+
+var nextQuestion = function() {
+    console.log("clicked an answer")
+};
+
 // logic.end-quiz
 var endQuiz = function() {
-    console.log("endQuiz() logic");
+    console.log("quiz ends");
 };
 
 // event-listeners
-startButtonEl.addEventListener("click", countdownTimer);
 startButtonEl.addEventListener("click", startQuiz);
