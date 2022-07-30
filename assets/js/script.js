13ab151c9c36c74dec677e684e9c6de28cf71837
escowin <edwin@escowinart.com>
2022-07-29 22:58:56 -0500

    display object array foundation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,60 @@ var rulesContainerEl = document.querySelector('#rules-container');
 var startButtonEl = document.querySelector('#start-button');
 var timerEl = document.querySelector('#timer');
 
-// data.user-input
 var inititals = "XX";
 var timer = 10;
 
-// data.questions-array
+// logic.questions
 var questions = [
     {
         question: "this is question 1",
-        answers: {
-            a: "answer a",
-            b: "answer b",
-            c: "answer c",
-            d: "answer d"
-        },
-        correct: "b"
+        answers: [
+            { text: "answer a", correct: true },
+            { text: "answer b", correct: true },
+            { text: "answer c", correct: true },
+            { text: "answer d", correct: false }
+        ]
     },
     {
         question: "this is question 2",
-        answers: {
-            a: "answer a",
-            b: "answer b",
-            c: "answer c",
-            d: "answer d"
-        },
-        correct: "a"
+        answers: [
+            { text: "answer a", correct: false },
+            { text: "answer b", correct: true },
+            { text: "answer c", correct: true },
+            { text: "answer d", correct: true }
+        ]
     },
 ];
+
+var displayQuestion = function(question) {
+    var questionsContainerEl = document.createElement("section");
+    questionsContainerEl.className = "container";
+    questionsContainerEl.setAttribute("id", "questions-container");
+
+    var questionEl = document.createElement("h2");
+    questionEl.className = "question";
+    questionEl.setAttribute("id", "question");
+    questionEl.innerText = question.question;
+
+    var answersEl = document.createElement("ul");
+    answersEl.className = "answers";
+
+    var answerEl = document.createElement("li");
+    answerEl.className = "answer";
+    answerEl.innerText = question.answers;
+
+    quizContainerEl.appendChild(questionsContainerEl);
+    questionsContainerEl.append(questionEl, answersEl);
+    answersEl.append(answerEl);
+};
+
+var setQuestion = function() {
+    for (var i = 0; i < questions.length; i++) {
+        var currentQuestion = questions[i];
+        console.log(currentQuestion.question);
+        displayQuestion(currentQuestion);
+    }
+};
 
 // logic.countdown-timer
 var countdownTimer = function() {
@@ -48,54 +75,12 @@ var countdownTimer = function() {
     }, 1000)
 };
 
-// logic.display-questions
-var displayQuestions = function() {
-    var questionsContainerEl = document.createElement("section");
-    questionsContainerEl.className = "container";
-    questionsContainerEl.setAttribute("id", "questions-container");
-
-    var questionEl = document.createElement("h2");
-    questionEl.className = "question";
-    questionEl.setAttribute("id", "question");
-    questionEl.innerText = questions.question;
-
-    var answersEl = document.createElement("ul");
-    answersEl.className = "answers";
-
-    var answerEl = document.createElement("li");
-    answerEl.className = "answer";
-    answerEl.innerText = questions.answer;
-
-    quizContainerEl.appendChild(questionsContainerEl);
-    questionsContainerEl.append(questionEl, answersEl);
-    answersEl.append(answerEl);
-
-    answerEl.addEventListener("click", nextQuestion);
-};
-
 // logic.start-quiz
 var startQuiz = function() {
     rulesContainerEl.remove();
-    getQuestion();
-    displayQuestions();
+    // getQuestion();
+    setQuestion();
     countdownTimer();
-};
-
-var getQuestion = function() {
-    for (var i = 0; i < questions.length; i++) {
-
-        if (questions.length > 0) {
-            console.log("works")
-        } else {
-            console.log("no questions");
-            break;
-        }
-    }
-
-    // bug
-    var currentQuestion = questions[i];
-        console.log(currentQuestion);
-
 };
 
 var nextQuestion = function() {
