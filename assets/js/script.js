cce65420db5e1af17f98aaac3f10bb89cd37e9bd
escowin <edwin@escowinart.com>
2022-08-31 14:35:50 -0500

    js:114 | shuffle questions array; pass current index into displayQuestion()

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,41 @@ var rulesContainerEl = document.querySelector('#rules-container');
 var startButtonEl = document.querySelector('#start-button');
 var timerEl = document.querySelector('#timer');
 
+// data.user-input
 var inititals = "XX";
+
+// data.questions array
+var questions = [
+    {
+        question: "This is an HTML question",
+        choices: [
+            { answer: "answer a", correct: true },
+            { answer: "answer b", correct: true },
+            { answer: "answer c", correct: true },
+            { answer: "answer d", correct: false }
+        ]
+    },
+    {
+        question: "this is question 2",
+        choices: [
+            { answer: "answer a", correct: false },
+            { answer: "answer b", correct: true },
+            { answer: "answer c", correct: true },
+            { answer: "answer d", correct: true }
+        ]
+    },
+    {
+        question: "this is question 3",
+        choices: [
+            { answer: "answer a", correct: false },
+            { answer: "answer b", correct: true },
+            { answer: "answer c", correct: true },
+            { answer: "answer d", correct: true }
+        ]
+    },
+];
+
+// data.scoreboard
 
 // logic.timer
 var timer = 10;
@@ -24,76 +58,86 @@ var countdownTimer = function() {
     }, 1000)
 };
 
-// logic.questions
-var questions = [
-    {
-        question: "This is an HTML question",
-        answers: [
-            { text: "answer a", correct: true },
-            { text: "answer b", correct: true },
-            { text: "answer c", correct: true },
-            { text: "answer d", correct: false }
-        ]
-    },
-    {
-        question: "this is question 2",
-        answers: [
-            { text: "answer a", correct: false },
-            { text: "answer b", correct: true },
-            { text: "answer c", correct: true },
-            { text: "answer d", correct: true }
-        ]
-    },
-];
+// shuffle questions
+var shuffledQuestions;
+var currentQuestionIndex;
 
 var displayQuestion = function(question) {
+    // <#quiz-container><h2 #question.../><#choices-container><.choice.../></></>
+    var currentQuestion = question.question;
+
+    // question container
     var questionsContainerEl = document.createElement("section");
+    questionsContainerEl.innerHTML = "";
     questionsContainerEl.className = "container";
     questionsContainerEl.setAttribute("id", "questions-container");
 
+    // displays current question
     var questionEl = document.createElement("h2");
     questionEl.className = "question";
     questionEl.setAttribute("id", "question");
-    questionEl.innerText = question.question;
+    questionEl.innerText = currentQuestion;
 
-    var answersEl = document.createElement("ul");
-    answersEl.className = "answers";
+    // loop through choices
+    question.answers.forEach(choices => {
+        var choiceEl = document.createElement("button");
+        choiceEl.innerText = choices.answer 
 
-    var answerEl = document.createElement("li");
-    answerEl.className = "answer";
-    answerEl.innerText = question.answers.text;
+        
+    })
+    console.log(questionEl);
 
+    questionsContainerEl.appendChild(questionEl);
     quizContainerEl.appendChild(questionsContainerEl);
-    questionsContainerEl.append(questionEl, answersEl);
-    answersEl.append(answerEl);
 
-    answerEl.addEventListener("click", nextQuestion);
+    // answers container
+    // var answersEl = document.createElement("ul");
+    // answersEl.className = "answers-container";
+
+    // displays answers as text
+    // question.choices.forEach(choices => {
+    //     var answerEl = document.createElement("li");
+    //     answerEl.className = "answer";
+    //     answerEl.innerText = choices.text;
+    
+    //     questionsContainerEl.append(questionEl, answersEl);
+    //     answersEl.append(answerEl);
+    // })
+    //     quizContainerEl.appendChild(questionsContainerEl);
+
+    // answerEl.addEventListener("click", nextQuestion);
 };
 
-var setQuestion = function() {
-    for (var i = 0; i < questions.length; i++) {
-        var currentQuestion = questions[i];
-        // **pause here**
-        // currently displays all index arrays
-        displayQuestion(currentQuestion);
-    }
-};
+var getQuestion = function() {
+    displayQuestion(shuffledQuestions[currentQuestionIndex]);
 
-var nextQuestion = function() {
-    console.log("clicked answer button")
+    // loop through questions array
+    // for (var i = 0; i < questions.length; i++) {
+    //     var currentQuestion = questions[i].question;
+    //     var currentChoices = questions[i].question.choices;
+    // }
+
+    // clickedAnswer();
 };
 
 // logic.start-quiz
 var startQuiz = function() {
     rulesContainerEl.remove();
-    // getQuestion();
-    setQuestion();
+    // shuffles questions array
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    console.log(shuffledQuestions);
+    currentQuestionIndex = 0;    
+    getQuestion();
     countdownTimer();
 };
 
 // logic.end-quiz
 var endQuiz = function() {
     console.log("quiz ends");
+    quizContainerEl.remove();
+
+    // display scoreboard
+    console.log("display scoreboard");
 };
 
 // event-listeners
