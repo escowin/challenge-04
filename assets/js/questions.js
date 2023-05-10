5fcfe5e4aea0b8c2222140f5ce5255cc2ecfab01
escowin <edwin@escowinart.com>
2023-05-10 14:15:17 -0500

    modularized script, used ternary to increase legibility, and fixed timer

---
+++ assets/js/questions.js
@@ -0,0 +1,68 @@
+// - questions
+let questions = [
+    {
+      question: "Which element does not need a corresponding closing tag?",
+      answers: [
+        { choice: "<link>", correct: true },
+        { choice: "<script>", correct: false },
+        { choice: "<html>", correct: false },
+        { choice: "<a>", correct: false },
+      ],
+    },
+    {
+      question:
+        "How does one remove leading & trailing white space from a specific element?",
+      answers: [
+        { choice: "Element.value.remove()", correct: false },
+        { choice: "Element.value.trim()", correct: true },
+        { choice: "Element.string.push()", correct: false },
+        { choice: "Element.text.remove()", correct: false },
+      ],
+    },
+    {
+      question: "Cage battle: Python v Java v PHP v Javascript. Who wins?",
+      answers: [
+        { choice: "Python", correct: false },
+        { choice: "Java", correct: false },
+        { choice: "PHP", correct: false },
+        { choice: "Javascript", correct: true },
+      ],
+    },
+    {
+      question: "Success is defined by:",
+      answers: [
+        { choice: "function foo() {};", correct: false },
+        { choice: "var foo = function () {};", correct: false },
+        { choice: "const foo = () => {};", correct: true },
+        { choice: "let food = () => {};", correct: true },
+      ],
+    },
+    {
+      question: "The key to a good template literal is to return with..",
+      answers: [
+        { choice: "single quotes ''", correct: false },
+        { choice: 'double quotes "" ', correct: false },
+        { choice: "back-ticks ``", correct: true },
+        { choice: "backslashes \u005c\u005c", correct: false },
+      ],
+    },
+    {
+      question: "'They know not what they do' refers to:",
+      answers: [
+        { choice: "copy & pasting answers from stackOverflow", correct: false },
+        {
+          choice:
+            "haphharzardly alternating between single and double quotes within javascript code",
+          correct: true,
+        },
+        { choice: "writing <divs> all the way down", correct: true },
+      ],
+    },
+    {
+      question: "Booleans are either true or false",
+      answers: [
+        { choice: "true", correct: true },
+        { choice: "false", correct: false },
+      ],
+    },
+  ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,19 @@
-// data
-// - dom elements
-const quizContainerEl = document.querySelector("#quiz-container");
-const rulesContainerEl = document.querySelector("#rules-container");
-const startButtonEl = document.querySelector("#start-button");
-const timerEl = document.querySelector("#timer");
-
+// variables
+const quizContainerEl = document.getElementById("quiz-container");
+const rulesContainerEl = document.getElementById("rules-container");
+const startButtonEl = document.getElementById("start-button");
+const timerEl = document.getElementById("timer");
 const questionContainerEl = document.createElement("section");
 const questionEl = document.createElement("h2");
+
 let currentQuestionIndex = 0;
-
-// - questions
-let questions = [
-  {
-    question: "Which element does not need a corresponding closing tag?",
-    answers: [
-      { choice: "<link>", correct: true },
-      { choice: "<script>", correct: false },
-      { choice: "<html>", correct: false },
-      { choice: "<a>", correct: false },
-    ],
-  },
-  {
-    question:
-      "How does one remove leading & trailing white space from a specific element?",
-    answers: [
-      { choice: "Element.value.remove()", correct: false },
-      { choice: "Element.value.trim()", correct: true },
-      { choice: "Element.string.push()", correct: false },
-      { choice: "Element.text.remove()", correct: false },
-    ],
-  },
-  {
-    question: "Cage battle: Python v Java v PHP v Javascript. Who wins?",
-    answers: [
-      { choice: "Python", correct: false },
-      { choice: "Java", correct: false },
-      { choice: "PHP", correct: false },
-      { choice: "Javascript", correct: true },
-    ],
-  },
-  {
-    question: "Success is defined by:",
-    answers: [
-      { choice: "function foo() {};", correct: false },
-      { choice: "var foo = function () {};", correct: false },
-      { choice: "const foo = () => {};", correct: true },
-      { choice: "let food = () => {};", correct: true },
-    ],
-  },
-  {
-    question: "The key to a good template literal is to return with..",
-    answers: [
-      { choice: "single quotes ''", correct: false },
-      { choice: 'double quotes "" ', correct: false },
-      { choice: "back-ticks ``", correct: true },
-      { choice: "backslashes \u005c\u005c", correct: false },
-    ],
-  },
-  {
-    question: "'They know not what they do' refers to:",
-    answers: [
-      { choice: "copy & pasting answers from stackOverflow", correct: false },
-      {
-        choice:
-          "haphharzardly alternating between single and double quotes within javascript code",
-        correct: true,
-      },
-      { choice: "writing <divs> all the way down", correct: true },
-    ],
-  },
-  {
-    question: "Booleans are either true or false",
-    answers: [
-      { choice: "true", correct: true },
-      { choice: "false", correct: false },
-    ],
-  },
-];
-
 let points = 0;
 let scoreboard = [];
 let initials = "";
+let time = questions.length * 10;
+let timer;
 
-// logic.current year
+// functions
 function currentYear() {
   year = new Date().getFullYear();
   const yearEl = document.getElementById("year");
@@ -94,22 +25,43 @@ function currentYear() {
   `);
 }
 
-// logic.timer
-// let time;
-let time = questions.length * 10;
-let timer;
+// quiz functions
+function startQuiz() {
+  // remove #rules-container from dom
+  quizContainerEl.innerHTML = "";
 
-function countdown() {
+  // create & post #question-container in its place
+  questionContainerEl.setAttribute("id", "question-container");
+  questionContainerEl.className = "container";
+  quizContainerEl.appendChild(questionContainerEl);
+
+  // randomize the questions array; pass the random array as an argument
+  let random = questions.sort(() => Math.random() - 0.5);
+  getQuestion(random);
+
+  // start the countdown timer
+  countdownTimer(time);
+  timer = setInterval(countdownTimer, 1000);
+}
+
+// quiz timer
+function countdownTimer() {
   time;
   time--;
 
-  if (time >= 10) {
-    timerEl.textContent = `${time}s`;
-  } else if (time < 10 && time >= 0) {
-    timerEl.textContent = `0${time}s`;
-  } else {
-    endQuiz(points);
-  }
+  // if (time >= 10) {
+  //   timerEl.textContent = `${time}s`;
+  // } else if (time < 10 && time >= 0) {
+  //   timerEl.textContent = `0${time}s`;
+  // } else {
+  //   endQuiz(points);
+  // }
+
+  time >= 10
+    ? (timerEl.textContent = `${time}s`)
+    : time < 10 && time >= 0
+    ? (timerEl.textContent = `0${time}s`)
+    : endQuiz(points);
 }
 
 // - get the current question from questions array
@@ -169,25 +121,6 @@ function nextQuestion(random) {
   } else {
     getQuestion(random);
   }
-}
-
-// logic.start-quiz
-function startQuiz() {
-  // remove #rules-container from dom
-  quizContainerEl.innerHTML = '';
-
-  // create & post #question-container in its place
-  questionContainerEl.setAttribute("id", "question-container");
-  questionContainerEl.className = "container";
-  quizContainerEl.appendChild(questionContainerEl);
-
-  // randomize the questions array; pass the random array as an argument
-  let random = questions.sort(() => Math.random() - 0.5);
-  getQuestion(random);
-
-  // start the countdown timer
-  // countdownTimer(time);
-  timer = setInterval(countdown, 1000);
 }
 
 // logic.end-quiz | removes question container; allows user to save score
@@ -282,14 +215,14 @@ function displayScoreboard() {
   const replayButtonEl = document.createElement("button");
   replayButtonEl.setAttribute("id", "replay-button");
   replayButtonEl.className = "button active";
-  replayButtonEl.textContent = "play again?"
-  replayButtonEl.addEventListener('click', startQuiz);
+  replayButtonEl.textContent = "play again?";
+  replayButtonEl.addEventListener("click", startQuiz);
 
   quizContainerEl.appendChild(replayButtonEl);
 
   // populate scoreboard with previous scores pulled from local storage
 }
 
-// event-listeners
+// calls
 currentYear();
 startButtonEl.addEventListener("click", startQuiz);
